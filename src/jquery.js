c772998cc87865f8d69f15862d3c0f2ab0b407d2
zhangyongwang <20225614@qq.com>
2019-10-28 08:28:24 +0800

    jQuery.prototype

--- src/jquery.js
+++ src/jquery.js
@@ -1,4 +1,4 @@
-window.jQuery = function (selectorOrArrayOrTemplate) {
+window.$ = window.jQuery = function (selectorOrArrayOrTemplate) {
     let elements = null
     if (typeof selectorOrArrayOrTemplate === 'string') {
         if (selectorOrArrayOrTemplate[0] === '<') {//创建
@@ -16,78 +16,83 @@ window.jQuery = function (selectorOrArrayOrTemplate) {
         return container.content.firstChild
     }
     // api 可以操作 elements
-    return {
-        jquery: true,
+    const api = Object.create(jQuery.prototype)
+    // api.elements = elements
+    // api.oldApi = selectorOrArrayOrTemplate.oldApi
+    Object.assign(api, {
         elements: elements,
-        get(index) {
-            return elements[index]
-        },
-        appendTo(node) {
-            if (node instanceof Element) {
-                this.each(el => node.appendChild(el))
-            } else if (node.jquery === true) {
-                this.each(el => node.get(0).appendChild(el))
-            }
-        },
-        append(children) {
-            if (children instanceof Element) {
-                this.get(0).appendChild(children)
-            } else if (children instanceof HTMLCollection) {
-                for (let i = 0; i < children.length; i++) {
-                    this.get(0).appendChild(children[i])
-                }
-            } else if (children.jquery === true) {
-                children.each(node => this.get(0).appendChild(node))
-            }
-        },
-        find(selector) {
-            let array = []
-            for (let i = 0; i < elements.length; i++) {
-                const elements2 = Array.from(elements[i].querySelectorAll(selector))
-                array = array.concat(elements2)
-            }
-            array.oldApi = this // this 是 旧的api
-            return jQuery(array)
-        },
-        each(fn) {
-            for (let i = 0; i < elements.length; i++) {
-                fn.call(null, elements[i], i)
-            }
-            return this
-        },
-        parent() {
-            const array = []
-            this.each(node => {
-                if (array.indexOf(node.parentNode) === -1) {
-                    array.push(node.parentNode)
-                }
-            })
-            return jQuery(array)
-        },
-        children() {
-            const array = []
-            this.each(node => {
-                if (array.indexOf(node.parentNode) === -1) {
-                    array.push(...node.children)
-                }
-            })
-            return jQuery(array)
-        },
-        print() {
-            console.log(elements)
-        },
-        addClass(className) {
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className)
-            }
-            return this
-        },
-        oldApi: selectorOrArrayOrTemplate.oldApi,
-        end() {
-            return this.oldApi // this 就是当前的 api 
-        }
-    }
+        oldApi: selectorOrArrayOrTemplate.oldApi
+    })
+    return api
 }
 
-
-window.$ = window.jQuery
+jQuery.prototype = {
+    // constructor: jQuery
+    jquery: true,
+    get(index) {
+        return elements[index]
+    },
+    appendTo(node) {
+        if (node instanceof Element) {
+            this.each(el => node.appendChild(el))
+        } else if (node.jquery === true) {
+            this.each(el => node.get(0).appendChild(el))
+        }
+    },
+    append(children) {
+        if (children instanceof Element) {
+            this.get(0).appendChild(children)
+        } else if (children instanceof HTMLCollection) {
+            for (let i = 0; i < children.length; i++) {
+                this.get(0).appendChild(children[i])
+            }
+        } else if (children.jquery === true) {
+            children.each(node => this.get(0).appendChild(node))
+        }
+    },
+    find(selector) {
+        let array = []
+        for (let i = 0; i < elements.length; i++) {
+            const elements2 = Array.from(elements[i].querySelectorAll(selector))
+            array = array.concat(elements2)
+        }
+        array.oldApi = this // this 是 旧的api
+        return jQuery(array)
+    },
+    each(fn) {
+        for (let i = 0; i < elements.length; i++) {
+            fn.call(null, elements[i], i)
+        }
+        return this
+    },
+    parent() {
+        const array = []
+        this.each(node => {
+            if (array.indexOf(node.parentNode) === -1) {
+                array.push(node.parentNode)
+            }
+        })
+        return jQuery(array)
+    },
+    children() {
+        const array = []
+        this.each(node => {
+            if (array.indexOf(node.parentNode) === -1) {
+                array.push(...node.children)
+            }
+        })
+        return jQuery(array)
+    },
+    print() {
+        console.log(elements)
+    },
+    addClass(className) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.add(className)
+        }
+        return this
+    },
+    end() {
+        return this.oldApi // this 就是当前的 api 
+    }
+}
